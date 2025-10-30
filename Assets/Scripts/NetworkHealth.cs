23e886c6d43003c936f9317bb62fb0a4488f3c86
Mukluk10 <pyry.urala@gmail.com>
2025-10-30 11:01:57 +0200

    changed health to networkvariable

--- Assets/Scripts/NetworkHealth.cs
+++ Assets/Scripts/NetworkHealth.cs
@@ -6,26 +6,33 @@ public class NetworkHealth : NetworkBehaviour
     [SerializeField]private int currentHealth;
     [SerializeField] private int maxHealth;
 
+
+    public NetworkVariable<int> _health = new NetworkVariable<int>
+        (100, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        currentHealth = maxHealth;
+        _health.Value = maxHealth;
     }
 
     [ServerRpc(RequireOwnership = false)]
 
     public void TakeDamageServerRpc(int damage)
     {
-            currentHealth -= damage;
+            _health.Value -= damage;
 
-            Debug.Log("Took Damage" + damage + "currentHealth is " + currentHealth);
+            Debug.Log("Took Damage" + damage + "currentHealth is " + _health.Value);
     }
 
     private void Update()
     {
-        if(currentHealth <= 0)
+        if(_health.Value <= 0)
         {
             this.NetworkObject.Despawn();
         }
     }
+
+    
 }
