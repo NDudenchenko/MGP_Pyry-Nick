4e3525ab39d3ce1d3cacf46e5cab1b0fca2fbe73
NDudenchenko <n.dudenchenko@gmail.com>
2025-12-15 19:05:24 +0200

    - Maybe respawn fixed

--- Assets/Scripts/NetworkHealth.cs
+++ Assets/Scripts/NetworkHealth.cs
@@ -31,12 +31,14 @@ public class NetworkHealth : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void ReSpawnServerRpc()
     {
-        if (respawnPoints.Length > 0)
-        {
-            int randSpawnIndex = Random.Range(0, respawnPoints.Length);
-            this.transform.position = respawnPoints[randSpawnIndex].transform.position;
-            _health.Value = maxHealth;
-        }
+        // if (respawnPoints.Length > 0)
+        // {
+        //     int randSpawnIndex = Random.Range(0, respawnPoints.Length);
+        //     this.transform.position = respawnPoints[randSpawnIndex].transform.position;
+        //     _health.Value = maxHealth;
+        // }
+        
+        _health.Value = maxHealth;
     }
 
     private void Update()
@@ -46,7 +48,17 @@ public class NetworkHealth : NetworkBehaviour
             // Instantiate(specMode, transform.position, Quaternion.identity);
             // this.NetworkObject.Despawn();
 
-            //ReSpawnServerRpc();
+            OnClientRespawn();
+        }
+    }
+
+    private void OnClientRespawn()
+    {
+        if (respawnPoints.Length > 0)
+        {
+            int randSpawnIndex = Random.Range(0, respawnPoints.Length);
+            this.transform.position = respawnPoints[randSpawnIndex].transform.position;
+            ReSpawnServerRpc();
         }
     }
 }
